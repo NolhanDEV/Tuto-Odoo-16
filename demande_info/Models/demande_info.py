e7e1b81efd507a35a1792073e75408bf22203409
Nolhan <nolhan.grandjean@gmail.com>
2023-03-17 17:12:49 +0100

    init

--- demande_info/Models/demande_info.py
+++ demande_info/Models/demande_info.py
@@ -9,3 +9,20 @@ class DemandeInformation(models.Model):
     mail = fields.Char(required=True)
     societe = fields.Char(required=True)
     tel = fields.Char(required=True)
+    description = fields.Char(required=False)
+    date = fields.Date(required=False, copy=False)
+    tag_ids = fields.Many2many("demande.information.tag", string="Tags")
+    statut = fields.Selection(
+        default='new',
+        selection=[('new', 'New'), ('en cours de traitement', 'En Cours De Traitement'), ('traité', 'Traité'),
+                   ('assigné', 'Assigné')],
+        required=True,)
+
+class DemandeInformationTag(models.Model):
+    _name = "demande.information.tag"
+    _description = "Demande Information Tag"
+
+    name = fields.Char(required=True)
+    color = fields.Integer(required=True)
+
+    _sql_constraints = [('unique_name', 'unique(name)', 'This Tag already exist')]
